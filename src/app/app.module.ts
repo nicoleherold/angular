8e6ca150224c31b487bc0ecb0381abbf7bbe498c
Nicole <nicole.herold@tamedia.ch>
2021-01-16 18:41:57 +0100

    MatInputModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+
+import {MatInputModule} from '@angular/material/input';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    MatInputModule,
+    NoopAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
